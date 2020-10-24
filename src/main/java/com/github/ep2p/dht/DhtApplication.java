d58157972f5d8f8f89728cfeb00bc01b959546fa
sepehr-gh <sepehrgh01@gmail.com>
2020-10-24 10:43:45 +0330

    Init project

---
+++ src/main/java/com/github/ep2p/dht/DhtApplication.java
@@ -0,0 +1,13 @@
+package com.github.ep2p.dht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ep2p/dht/DhtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ep2p.dht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
